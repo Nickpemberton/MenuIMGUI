e0eb6f5afd3ed0c1b30ad41769b1066c73a40c32
Nick <48014022+Nickpemberton@users.noreply.github.com>
2023-03-18 11:56:09 +1100

    Added Health

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -1,31 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Palmmedia.ReportGenerator.Core.Reporting.Builders;
 using UnityEngine;
 
 namespace Player
 {
     public class Shoot : MonoBehaviour
     {
+        public bool gunActive;
+        public float bulletDrop;
+        public GameObject bulletHole;
         void Update()
         {
-            if (Input.GetKey(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Alpha1) && gunActive == false)
+            {
+                Debug.Log("Active");
+                gunActive = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Debug.Log("Deactivated");
+                gunActive = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && gunActive)
             {
                 Ray bullet;
                 bullet = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
 
                 RaycastHit hitInfo;
+                
+                
+                
 
                 if (Physics.Raycast(bullet, out hitInfo, 500))
                 {
                     if (hitInfo.collider.tag == "NPC")
                     {
-                        hitInfo.collider.gameObject.SetActive(false);
-                        Debug.Log("Active");
+                        hitInfo.collider.gameObject.GetComponent<NPC.NpcHealth>().RemoveHealth(50);
+                        Debug.Log("Shot a bullet");
                     }
                 }
 
             }
         }
-            
     }
 }
